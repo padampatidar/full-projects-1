016a43dd2688d30f98012813a6e2f152a65259f6
padampatidar <132297116+padampatidar@users.noreply.github.com>
2023-05-30 16:24:31 +0530

    Create MyFetchAPI.tsx

---
+++ React-native/API/src/normal/MyFetchAPI.tsx
@@ -0,0 +1,5 @@
+// export const myFetchGetRequest = async () => {
+//   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const resJson = await response.json();
+//   return resJson;
+// };
